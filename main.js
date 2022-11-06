0a7f5dcb8f6f6ab6c1d708446b0b72830e8f0a49
pigsugar <101998778+pigsugar@users.noreply.github.com>
2022-11-06 12:42:24 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality:90
+});
+
+camera= document.getElementById("camera");
+Webcam.attach('#camera')
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){ 
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+
+    })
+
+}
+ console.log('ml5:',ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/afjJ8rlHV/model.json', modelloaded);
+function modelloaded() {
+    console.log('model Loaded!');
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 =toSpeak
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1);
+    synth.speak(utterThis)
+
+}
